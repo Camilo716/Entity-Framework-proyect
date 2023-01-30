7eb40159c9b56c02f54eaea4101c648c84e6995d
Camilo <camilo.gonzalez.col@gmail.com>
2023-01-30 16:32:16 -0500

    Proyect conected with postrgres

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 using APITask.Context;
-using APITask.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<APITaskContext>(opt => opt.UseInMemoryDatabase("taskDb"));
+//builder.Services.AddDbContext<APITaskContext>(opt => opt.UseInMemoryDatabase("taskDb"));
+builder.Services.AddNpgsql<APITaskContext>(builder.Configuration.GetConnectionString("TaskDb"));
 
 var app = builder.Build();
 
@@ -18,5 +18,5 @@ app.MapGet("/dbconection", async ([FromServices] APITaskContext dbContext) =>
     return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
 });
 
-app.Run();
+app.Run("https://localhost:7157");
 
